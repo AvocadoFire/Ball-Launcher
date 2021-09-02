d69cb6658616e4aee8c491f59bad7deedb85f0d3
Laurie <elaurllc@gmail.com>
2021-09-01 21:39:14 -0400

    Fixing Ball

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,28 +6,56 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D currentBallRigidbody;
+    [SerializeField] GameObject ballPrefab;
+    [SerializeField] Rigidbody2D pivot;
+    [SerializeField] float detachTime = .1f;
+    [SerializeField] float respawnDelay = 1f;
+
+    Rigidbody2D currentBallRigidbody;
+    SpringJoint2D currentBallSpringJoint;
+
     private Camera mainCamera;
+    private bool isDragging;
 
     private void Start()
     {
-        mainCamera = Camera.main;
+       mainCamera = Camera.main;
     }
 
     void Update()
     {
+        if(currentBallRigidbody == null){ return; }
 
         if (!Touchscreen.current.primaryTouch.press.isPressed) 
             //called every frame but if finger not on screen there is no value so have to do if return
         { 
-            currentBallRigidbody.isKinematic = false;
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+            isDragging = false;
             return;
         }
+        isDragging = true;
         currentBallRigidbody.isKinematic = true;
 
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
         Vector2 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
         currentBallRigidbody.position = worldPosition;
+    }
 
+    private void LaunchBall()
+    {
+        currentBallRigidbody.isKinematic = false; ///make body react to physics again
+        currentBallRigidbody = null; //doesn't matter if touch screen
+
+        Invoke(nameof(DetachBall), detachTime);
+       
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
