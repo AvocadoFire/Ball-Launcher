eb81ab73a34e0e35af2f64d527bd1012820bf018
Laurie <elaurllc@gmail.com>
2021-09-02 12:09:59 -0400

    Camera and Tweaks

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -8,7 +8,7 @@ public class BallHandler : MonoBehaviour
 {
     [SerializeField] GameObject ballPrefab;
     [SerializeField] Rigidbody2D pivot;
-    [SerializeField] float detachTime = .1f;
+    [SerializeField] float detachDelay = .1f;
     [SerializeField] float respawnDelay = 1f;
 
     Rigidbody2D currentBallRigidbody;
@@ -20,6 +20,7 @@ public class BallHandler : MonoBehaviour
     private void Start()
     {
        mainCamera = Camera.main;
+       SpawnNewBall();
     }
 
     void Update()
@@ -44,13 +45,21 @@ public class BallHandler : MonoBehaviour
         currentBallRigidbody.position = worldPosition;
     }
 
+    private void SpawnNewBall()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, pivot.position, Quaternion.identity);
+        currentBallRigidbody = ballInstance.GetComponent<Rigidbody2D>();
+        currentBallSpringJoint = ballInstance.GetComponent<SpringJoint2D>();
+        currentBallSpringJoint.connectedBody = pivot;
+    }
+
     private void LaunchBall()
     {
         currentBallRigidbody.isKinematic = false; ///make body react to physics again
         currentBallRigidbody = null; //doesn't matter if touch screen
 
-        Invoke(nameof(DetachBall), detachTime);
-       
+        Invoke(nameof(DetachBall), detachDelay);
+        Invoke(nameof(SpawnNewBall), respawnDelay);
     }
 
     private void DetachBall()
